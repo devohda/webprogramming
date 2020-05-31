1ac54f5b39a412eff5496e71de7b6a6a0006cabb
jeongeun99 <pje9912@naver.com>
2020-05-31 23:17:33 +0900

    카드 세부페이지 수정

---
+++ 팀플/js/card.js
@@ -0,0 +1,56 @@
+function dis(){
+    if($('#dis1').css('display') == 'none'){
+    $('#dis1').show();
+     }
+     else{
+    $('#dis1').hide();
+    }
+}
+function dis(){
+    if($('#dis2').css('display') == 'none'){
+    $('#dis2').show();
+     }
+     else{
+    $('#dis2').hide();
+    }
+}
+function dis(){
+    if($('#dis3').css('display') == 'none'){
+    $('#dis3').show();
+     }
+     else{
+    $('#dis3').hide();
+    }
+}
+function dis(){
+    if($('#dis4').css('display') == 'none'){
+    $('#dis4').show();
+     }
+     else{
+    $('#dis4').hide();
+    }
+}
+function dis(){
+    if($('#dis5').css('display') == 'none'){
+    $('#dis5').show();
+     }
+     else{
+    $('#dis5').hide();
+    }
+}
+function dis(){
+    if($('#dis6').css('display') == 'none'){
+    $('#dis6').show();
+     }
+     else{
+    $('#dis6').hide();
+    }
+}
+function dis(){
+    if($('#dis7').css('display') == 'none'){
+    $('#dis7').show();
+     }
+     else{
+    $('#dis7').hide();
+    }
+}
