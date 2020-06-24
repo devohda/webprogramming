e336dfbd1e23e95c5fd75a6fb987ebc89f8b6494
devohda <fffvvv6@gmail.com>
2020-06-24 15:30:21 +0900

    주석

--- teamproject/js/calculator.js
+++ teamproject/js/calculator.js
@@ -4,11 +4,11 @@ let save = document.getElementById('save').value;
 let term = document.getElementById('term').value;
 
 cal.addEventListener("click", function calculate() {
-    let price = document.getElementById('product_price').value;
-    let save = document.getElementById('save').value;
-    let term = document.getElementById('term').value;
+    let price = document.getElementById('product_price').value; //가격 입력 받기
+    let save = document.getElementById('save').value; //월별 저금 금액 입력 받기
+    let term = document.getElementById('term').value; //예상 저금 기간 입력 받기
     if (save) {
-        if (!isNaN(price / save)) {
+        if (!isNaN(price / save)) { //계산해서 저금 기간 반환
             let cal_term = document.getElementById('cal_term');
             document.getElementById('cal_term').innerHTML = price / save;
             console.log(price / save);
@@ -23,7 +23,7 @@ cal.addEventListener("click", function calculate() {
     }
 
     if (term) {
-        if (!isNaN(price / term)) {
+        if (!isNaN(price / term)) { //계산해서 월별 저금 금액 반환
             let cal_price = document.getElementById('cal_price');
             document.getElementById('cal_price').innerHTML = price / term;
             console.log(price / term);
