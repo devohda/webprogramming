c8398b9e4f1596c6b42563171c8b5db9aa4cf840
jeongeun99 <pje9912@naver.com>
2020-06-27 03:43:06 +0900

    주석

--- teamproject/js/card.js
+++ teamproject/js/card.js
@@ -1,80 +1,81 @@
 function dis1(){
-    if($('#dis1').css('display') == 'none'){
-    $('#dis1').show();
+    if($('#dis1').css('display') == 'none'){    //id가 dis1인 div가 none일 때(안보일 때) onclick되면
+    $('#dis1').show();                             //dis1 div 보여주기 
      }
-     else{
-    $('#dis1').hide();
+     else{                                             //none이 아니면
+    $('#dis1').hide();                              //dis1 div 숨기기
     }
 }
 function dis2(){
-    if($('#dis2').css('display') == 'none'){
-    $('#dis2').show();
+    if($('#dis2').css('display') == 'none'){    //id가 dis2인 div가 none일 때(안보일 때) onclick되면
+    $('#dis2').show();                             //dis2 div 보여주기 
      }
-     else{
-    $('#dis2').hide();
+     else{                                             //none이 아니면
+    $('#dis2').hide();                              //dis2 div 숨기기
     }
 }
 function dis3(){
-    if($('#dis3').css('display') == 'none'){
-    $('#dis3').show();
+    if($('#dis3').css('display') == 'none'){    //id가 dis3인 div가 none일 때(안보일 때) onclick되면
+    $('#dis3').show();                             //dis3 div 보여주기 
      }
-     else{
-    $('#dis3').hide();
+     else{                                            //none이 아니면
+    $('#dis3').hide();                              //dis3 div 숨기기
     }
 }
 function dis4(){
-    if($('#dis4').css('display') == 'none'){
-    $('#dis4').show();
+    if($('#dis4').css('display') == 'none'){    //id가 dis4인 div가 none일 때(안보일 때) onclick되면
+    $('#dis4').show();                             //dis4 div 보여주기 
      }
-     else{
-    $('#dis4').hide();
+     else{                                           //none이 아니면
+    $('#dis4').hide();                              //dis4 div 숨기기
     }
 }
 function dis5(){
-    if($('#dis5').css('display') == 'none'){
-    $('#dis5').show();
+    if($('#dis5').css('display') == 'none'){    //id가 dis5인 div가 none일 때(안보일 때) onclick되면
+    $('#dis5').show();                            //dis5 div 보여주기 
      }
-     else{
-    $('#dis5').hide();
+     else{                                           //none이 아니면
+    $('#dis5').hide();                              //dis5 div 숨기기
     }
 }
 function dis6(){
-    if($('#dis6').css('display') == 'none'){
-    $('#dis6').show();
+    if($('#dis6').css('display') == 'none'){    //id가 dis6인 div가 none일 때(안보일 때) onclick되면
+    $('#dis6').show();                            //dis6 div 보여주기 
      }
-     else{
-    $('#dis6').hide();
+     else{                                           //none이 아니면
+    $('#dis6').hide();                              //dis6 div 숨기기
     }
 }
 function dis7(){
-    if($('#dis7').css('display') == 'none'){
-    $('#dis7').show();
+    if($('#dis7').css('display') == 'none'){    //id가 dis7인 div가 none일 때(안보일 때) onclick되면
+    $('#dis7').show();                            //dis7 div 보여주기 
      }
-     else{
-    $('#dis7').hide();
+     else{                                          //none이 아니면
+    $('#dis7').hide();                             //dis7 div 숨기기
     }
 }
 function dis8(){
-    if($('#dis8').css('display') == 'none'){
-    $('#dis8').show();
+    if($('#dis8').css('display') == 'none'){    //id가 dis8인 div가 none일 때(안보일 때) onclick되면
+    $('#dis8').show();                            //dis8 div 보여주기 
      }
-     else{
-    $('#dis8').hide();
+     else{                                        //none이 아니면
+    $('#dis8').hide();                             //dis8 div 숨기기
     }
 }
 function dis9(){
-    if($('#dis9').css('display') == 'none'){
-    $('#dis9').show();
+    if($('#dis9').css('display') == 'none'){    //id가 dis9인 div가 none일 때(안보일 때) onclick되면
+    $('#dis9').show();                            //dis9 div 보여주기 
      }
-     else{
-    $('#dis9').hide();
+     else{                                       //none이 아니면
+    $('#dis9').hide();                             //dis9 div 숨기기
     }
 }
 function dis10(){
-    if($('#dis10').css('display') == 'none'){
-    $('#dis10').show();
+    if($('#dis10').css('display') == 'none'){    //id가 dis10인 div가 none일 때(안보일 때) onclick되면
+    $('#dis10').show();                           //dis10 div 보여주기 
      }
-     else{
-    $('#dis10').hide();
+     else{                                       //none이 아니면
+    $('#dis10').hide();                             //dis10 div 숨기기
     }
 }
+//각각의 div들이 개별적으로 보여졌다 숨겨졌다 해야하기때문에 다른 id를 매겨줌
